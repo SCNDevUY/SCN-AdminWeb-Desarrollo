cc52ee4eba8cf0d83697ad7a279f2ee6e13bf767
Alejandro ACER <scann258@hotmail.com>
2020-07-02 11:23:48 -0300

    agregado en config link de heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,6 @@
 
 export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'scndesarrollo.herokuapp.com';
 
 export const CLIENT_ID = '8897900251-qu1pjna9idq8trtc19f1qvsml0ofu8gf.apps.googleusercontent.com';
 
